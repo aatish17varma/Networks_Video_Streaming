49146eb714e066f37005330f53fa965f36d70a00
Aatish Varma <aatishvarma@10-16-221-182.dynapool.wireless.nyu.edu>
2018-12-11 13:08:25 -0500

    working - tings

--- server.py
+++ server.py
@@ -22,23 +22,29 @@ server.listen(10)
 
 (connection, address) = server.accept()
 
+data = b''
+
 data_size = struct.calcsize("L")
 
 while True:
   while len(data) < data_size:
-    data += connection.recv(4096).decode()
+    data += connection.recv(4096)
   packed_msg_size = data[:data_size]
   data = data[data_size:]
   msg_size = struct.unpack("L", packed_msg_size)[0]
   while len(data) < msg_size:
-      data += conn.recv(4096)
+      data += connection.recv(4096)
   frame_data = data[:msg_size]
   data = data[msg_size:]
   
 
   frame=pickle.loads(frame_data)
   print(frame)
-  cv2.imshow('frame',frame)  
+  cv2.imshow('frame',frame)
+
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+
 
   # print(sys.getsizeof(data))
 
